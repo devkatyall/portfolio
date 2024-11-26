9705907071f6688fb9e4cdf60a56d287f3e0a385
Dev <devkatyal@protonmail.com>
2024-11-26 02:42:13 -0500

    changes to hero sec

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function HeroSection() {
   return (
     <div id='aboutme' className='hero' >
-        <h1 className='h1 hero-t' >
-            Code Meets Curiosity, Data Fuels Discovery.
+        <h1 className='h1 hero-t'>
+            Crafting Code,<br /> Building Futures.
         </h1>
-        <div className='h2' >
-            <p className='hero-base' >
-                Hi, <span>I&#39;m Dev Katyal</span> — a data whisperer and digital creator who sees stories where others see spreadsheets. I don&#39;t just analyze; I architect insights, weaving them seamlessly into frontend elegance, backend brilliance, and mobile fluidity. From untangling data mysteries to engineering user-centric solutions, I transform complexity into clarity — making every byte count.
+        <div className='h2'>
+            <p className='hero-base'>
+                Hi, <span>I&#39;m Dev Katyal</span> — a web architect and digital creator who turns ideas into immersive online experiences. I don&#39;t just code; I design and develop, blending frontend finesse, backend brilliance, and responsive fluidity. From sleek landing pages to robust web applications, I transform visions into vibrant, user-centric realities — making every line of code matter.
             </p>
         </div>
     </div>
